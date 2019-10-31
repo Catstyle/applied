78e0e9d1dcf8ad59382511f76f04f4f9cb0afba3
Catstyle <Catstyle.Lee@gmail.com>
2019-10-31 10:55:25 +0800

    refined README/setup classifier

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ if __name__ == '__main__':
             'Topic :: Software Development :: Build Tools',
 
             # Pick your license as you wish (should match "license" above)
-            'License :: OSI Approved :: GPLv3 License',
+            'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
             # Specify the Python versions you support here.
             # In particular, ensure that you indicate whether you support
